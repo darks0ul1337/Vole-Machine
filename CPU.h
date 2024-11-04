4ca6ae6493813e8a54afe0a3aceddbca9ea88edc
Dark <153874724+darks0ul1337@users.noreply.github.com>
2024-11-04 02:12:36 +0200

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -16,6 +16,9 @@ public:
     string DecToHex(int num); // Convert decimal to hexadecimal.
     void Add(int idReg1, int idReg2, int idReg3, Memory& Register); // Add binary values in registers.
     void Add(Memory& Register, int idxReg1, int idxReg2, int idxReg3); // Add floating-point values in registers.
+    void OR(Memory& Register, int idxReg1, int idxReg2, int idxReg3);
+    void AND(Memory& Register, int idxReg1, int idxReg2, int idxReg3);
+    void XOR(Memory& Register, int idxReg1, int idxReg2, int idxReg3);
     bool isValid(string inst); // Check if a string is a valid hexadecimal.
 };
 
@@ -28,6 +31,7 @@ public:
     void Load(Memory& memory, Memory& Register, int idxReg, int idxMem); // Load data from memory to register.
     void Load(Memory& Register, int idxReg, string idxMem); // Load immediate value to register.
     void Jump(Memory& Register, int& counter, int idxReg, int idxMem); // Conditional jump based on register value.
+    void rotate(Memory& Register, int idxReg, int rotations);
     void halt(); // Halt program execution.
 };
 
